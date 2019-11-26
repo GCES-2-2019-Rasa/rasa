573d804fc823b3dbdf4718c4a8bcdaf9204cbb7b
LucasLermen <lucas.arthur.lermen@gmail.com>
2019-11-26 14:06:36 -0300

    Fix variable names

--- rasa/core/training/data.py
+++ rasa/core/training/data.py
@@ -1,21 +1,21 @@
 # noinspection PyPep8Naming
 class DialogueTrainingData:
-    def __init__(self, X, y, true_length=None):
-        self.X = X
+    def __init__(self, variables, y, true_length=None):
+        self.variables = variables
         self.y = y
         self.true_length = true_length
 
     def limit_training_data_to(self, max_samples):
-        self.X = self.X[:max_samples]
+        self.variables = self.variables[:max_samples]
         self.y = self.y[:max_samples]
         self.true_length = self.true_length[:max_samples]
 
     def is_empty(self):
         """Check if the training matrix does contain training samples."""
-        return self.X.shape[0] == 0
+        return self.variables.shape[0] == 0
 
     def max_history(self):
-        return self.X.shape[1]
+        return self.variables.shape[1]
 
     def num_examples(self):
         return len(self.y)
@@ -23,8 +23,8 @@ class DialogueTrainingData:
     def shuffled_X_y(self):
         import numpy as np
 
-        idx = np.arange(self.num_examples())
-        np.random.shuffle(idx)
-        shuffled_X = self.X[idx]
-        shuffled_y = self.y[idx]
-        return shuffled_X, shuffled_y
+        idVariables = np.arange(self.num_examples())
+        np.random.shuffle(idVariables)
+        shuffled_variables = self.variables[idVariables]
+        shuffled_y = self.y[idVariables]
+        return shuffled_variables, shuffled_y
